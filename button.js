68d7c43e6228a46ed2d64ae7e97c7fc6eeb6e17a
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-19 17:15:33 +0600

    active button is added and explained well

--- button.js
+++ button.js
@@ -1,8 +1,33 @@
+// the below system collected from youtube. It's very 
+// effective and efficient system than W3 schools' documention.
+// At this issue W3 schools' explanation is more complex. 
+
+// const buttons = document.querySelectorAll(".button-active");
+// buttons.forEach(button => {
+//     button.addEventListener("click", function () {
+//         buttons.forEach(btn => btn.classList.remove("active"));
+//         this.classList.add("active");
+//     })
+// })
+
+
+
+
+// I modify the above system at my own system.
+
 const buttons = document.getElementsByClassName("button-active");
-for (let btn of buttons) {
-    btn.addEventListener("click", function (event) {
-        const buttonBum = event.target;
-        buttonBum.style.backgroundColor = "red";
-        buttonBum.style.color = "white";
+for (let button of buttons) {
+    button.addEventListener("click", function (event) {
+        for (let btn of buttons) {
+            // At first I removed the active class dynamically from 
+            // all buttons which we got through loop.
+            btn.classList.remove("active");
+            // At second we add the active class to the specific button on which we clicked 
+            // by using this keyword. 
+            this.classList.add("active");
+        }
+        console.log(event.target);
     })
 }
+
+
