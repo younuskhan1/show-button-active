9fe004565ad4ad494836d5534e0c501e9eafb6ce
younuskhan1 <md.younuskhan412@gmail.com>
2024-03-01 22:34:45 +0600

    new system is added

--- button.js
+++ button.js
@@ -1,5 +1,5 @@
 // the below system collected from youtube. It's very 
-// effective and efficient system than W3 schools' documention.
+// effective and efficient system than W3 school documents.
 // At this issue W3 schools' explanation is more complex. 
 
 // const buttons = document.querySelectorAll(".button-active");
@@ -13,7 +13,7 @@
 
 
 
-// I modify the above system at my own system.
+// First rule: I modify the above system at my own system.
 
 const buttons = document.getElementsByClassName("button-active");
 // For first time I am running loop on buttons to add EventListener to each button 
@@ -30,9 +30,24 @@ for (let button of buttons) {
             // currently clicked button element
             this.classList.add("active");
         }
-        const targetButton = event.target;
-        console.log(targetButton);
+        // const targetButton = event.target;
+        // console.log(targetButton);
     })
 }
 
 
+// Second rule: another fantastic system to show button active 
+
+//  I have stored the below commented codes for future using to show that 
+//  which button is active among many buttons. 
+
+// const buttons = document.querySelectorAll(".button-active");
+
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", () => {
+//       buttons.forEach((button) => {
+//         button.classList.remove("active");
+//       });
+//       buttons[i].classList.add("active");
+//     });
+// }
